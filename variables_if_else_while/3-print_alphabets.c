9c0911ec80cb83855f0ccd8e4608bef89e53c497
carlfrank <carl-frank7@hotmail.com>
2023-06-20 15:13:52 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  *main - prints the alphabet in lowercase, and then in uppercase
  *Return: Always return 0
-/*
+*/
 
 int main(void)
 {
-char alpha:
+char alpha;
 for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
 putchar(alpha);
-
-for (alpha = 'A'; alpha <= 'Z'; alpha++)
-putchar(alpha);
+if (alpha == 'z')
+{
+	char ALPHA;
+for (ALPHA = 'A'; ALPHA <= 'Z'; ALPHA++)
+	putchar(ALPHA);
+}
+}
 putchar('\n');
 
 return(0);
