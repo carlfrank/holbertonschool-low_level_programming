4de66a35d91d69e4a6bce14d1f83b3a17e4dfc0c
carlfrank <carl-frank7@hotmail.com>
2023-06-20 15:43:52 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Print all the letters except q and e
+ *Return: Always return 0
+*/
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if (alpha != 'e'&& alpha != 'q')
+	putchar(alpha);
+
+}
+putchar('\n');
+return (0);
+}
