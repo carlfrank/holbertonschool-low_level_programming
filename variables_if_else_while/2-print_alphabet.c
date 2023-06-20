2b0cd3651d918cc0f782a49b32465fd4f604bbc1
carlfrank <carl-frank7@hotmail.com>
2023-06-20 14:12:58 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-/**
-*main- prints the alphabet in lowercase
-*Return- always return 0
-*/
-int main(void)
 
+/**
+*main - prints the alphabet in lowercase
+*
+*Return:  always return 0
+*/
+
+int main(void)
 {
 	char low;
 
-	for (low= 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 	putchar('\n');
 	return(0);
