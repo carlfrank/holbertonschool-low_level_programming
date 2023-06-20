9bf551c0c8dfa6f988260e0b0d6048cb401a1c0a
carlfrank <carl-frank7@hotmail.com>
2023-06-20 17:51:11 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - prints all single digit numbers
+ *Return: Always return 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
