15cd6c0284f44d528f20954ccfcc184fd20dc7bf
carlfrank <carl-frank7@hotmail.com>
2023-06-21 18:38:17 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - the alphabet, in lowercase
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z')
+{
+	_putchar(low);
+}
+	_putchar('\n');
+	return (0);
+}
