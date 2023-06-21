a864e6583f98401bf73bbced95ac2da35f1b6d49
carlfrank <carl-frank7@hotmail.com>
2023-06-21 17:17:40 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*main - Print putchar
+*Return: Always print 0
+*/
+
+int main(void)
+{
+char *x = "_putchar";
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}
+	_putchar('\n');
+	return (0);
+}
