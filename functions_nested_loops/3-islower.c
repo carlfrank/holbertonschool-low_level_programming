22cb30873ca4fd96639134e248d2a69f38e8ad89
carlfrank <carl-frank7@hotmail.com>
2023-06-22 12:58:52 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check for lowercase character
+ * _islower - check for lowercase character
  *@c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
