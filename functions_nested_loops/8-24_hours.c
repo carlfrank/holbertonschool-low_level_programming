b24f8a1c5a46efe8ca4554402d190a61a4b72a69
carlfrank <carl-frank7@hotmail.com>
2023-06-22 23:06:47 +0000

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- * jack bauer- prints every minute of the day of Jack Bauer
- * Return: ...
- */
+* jack_bauer - 24 hour clock
+*
+* Return: Full countdown of the countdown
+*/
 
 void jack_bauer(void)
 {
-int hours = 0, hours minutes = 0;
+	int hours = 0, minutes = 0;
 
-for (hours = 0; hours < 24; hours++)
-{
-	for (minutes = 0; minutes < 60; minutes++)
+	for (hours = 0; hours < 24; hours++)
 	{
-		_putchar((hours / 10) + '0');
-		_putchar((hours % 10) + '0');
-		_putchar(':');
-		_putchar((minutes / 10) + '0');
-		_putchar((minutes % 10) + '0');
-		_putchar('\n');
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
 	}
-}
 }
