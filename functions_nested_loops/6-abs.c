b94f6da043253fc601a695344a37ee779484957a
carlfrank <carl-frank7@hotmail.com>
2023-06-22 16:56:38 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int_abs- computes the absolute value of an integer
+ * @c: The character to be checked
+ * Return: absolute value of number of zero
+ */
+int_abs(int n)
+{
+    if (n < 0)
+	}
+    return -n;
+    }
+	else
+    return n;
+}
