47177b8ec904c6d5a21aa82dd7fb987bd8ca9beb
carlfrank <carl-frank7@hotmail.com>
2023-06-22 13:16:46 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character 
+ * *@c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+int _isalpha(char c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
