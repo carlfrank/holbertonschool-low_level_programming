37492c3fb6746493cb32672bba7d78bde82fd023
carlfrank <carl-frank7@hotmail.com>
2023-06-23 19:09:30 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_mist_numbers - Print the numbers since 0 up to 9
+ * Description: Prints the numbers excluding 2 and 4
+ * Return: the numbers since 0 up to 9
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+	
+	for (; x <= 9; x++)
+	{
+	continue;
+	}
+	else
+	{
+	putchar(x + '0');
+	}
+	}
+	_puctchar('\n');
+	}
