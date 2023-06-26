2c3bc3ebbc69958ccedf5892333f282efa16ef84
carlfrank <carl-frank7@hotmail.com>
2023-06-26 21:50:00 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,10 @@ void rev_string(char *s)
 	int len = 0, index = 0;
 	char tmp;
 
-	while (index = len - 1; index >= len / 2; index--)
+	while (s[index++])
+	len++;
+
+	for (index = len - 1; index >= len / 2; index--)
 	{
 	tmp = s [index];
 	s [index];
