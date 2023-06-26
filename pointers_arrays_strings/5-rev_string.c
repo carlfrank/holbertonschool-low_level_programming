42a55a20d2c7904463427eb309963d9f04e5e332
carlfrank <carl-frank7@hotmail.com>
2023-06-26 21:55:02 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,8 @@ void rev_string(char *s)
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
-	tmp = s [index];
-	s [index] = s [len - index - 1];
-	s [len - index - 1] = tmp;
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
