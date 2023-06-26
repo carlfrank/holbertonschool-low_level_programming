af7310c3201b65ce8b4781d60bf6780018bcbe82
carlfrank <carl-frank7@hotmail.com>
2023-06-26 15:14:20 +0000

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
+#ifndef MAIN
+#define MAIN
 /**
  * headerfile
  */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,3 +15,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+#endif
