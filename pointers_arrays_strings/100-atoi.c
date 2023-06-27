a5919f9fa1f64fdcc8a17e322aefad5ecb582a78
carlfrank <carl-frank7@hotmail.com>
2023-06-27 16:52:07 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,3 +2,33 @@
 
 /**
  * _atoi - convert a string to an integer
+ * @s: the pointer to convert
+ * Return: A integer
+ */
+{
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[c])
+	{
+	if (s[c] == 45)
+	{
+	main *= -1;
+	}
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
+}
