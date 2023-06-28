88b1b99de7b60a9663c81c770a8d7c3631133827
carlfrank <carl-frank7@hotmail.com>
2023-06-28 17:32:27 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,9 @@ char *_strcat(char *dest, char *src)
 {
 	int i, c;
 
-	for (i = 0; dest[i] != '\n'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 
-	for (c = 0; src[c] != 0; c++)
+	for (c = 0; src[c] != '\0'; c++)
 	{
 		dest[i] = src[c];
 		i++;
