4832ebb5c8a1a9bad21af421dfd18264e2c8c32d
carlfrank <carl-frank7@hotmail.com>
2023-06-29 10:58:09 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ _strcmp - Compare two strings
+ @s1: string
+ @s2: string
+ Return: negative int if s1 < s2, 0 if matching, int positive if s1 > s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'|| s2[i] != '\0' i++)
+	{
+		if (s[1] != s[2])
+			return (s1[i] - s[2])
+	}
+	return (0)
+}
