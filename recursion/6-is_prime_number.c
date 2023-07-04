64529eaa05dc611b9c1d33da518c0dd4df7c0171
carlfrank <carl-frank7@hotmail.com>
2023-07-04 21:56:05 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,21 +4,21 @@
 int is_prime(int n, int divisor)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 	if (n == 2 || n == 3)
-		return 1;
+		return (1);
 	if (n % divisor == 0)
-		return 0;
+		return (0);
 	if (divisor * divisor > n)
-		return 1;
-	return is_prime(n, divisor + 2);
+		return (1);
+	return (is_prime(n, divisor + 2));
 }
 
 int is_prime_number(int n)
 {
 	if (n == 2 || n == 3)
-		return 1;
+		return (1);
 	if (n % 2 == 0)
-		return 0;
-	return is_prime(n, 3);
+		return (0);
+	return (is_prime(n, 3));
 }
