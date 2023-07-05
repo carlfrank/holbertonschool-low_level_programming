05af1a22310376b7aaa0bf1c7d36d0930427eda7
carlfrank <carl-frank7@hotmail.com>
2023-07-05 14:48:56 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+
 int main(int argc, char* argv[])
 {
 	int i;
