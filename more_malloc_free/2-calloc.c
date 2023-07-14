21ba14a801681bfd0422503ffae5b4b03bb6f876
carlfrank <carl-frank7@hotmail.com>
2023-07-14 01:33:18 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,17 +11,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	char *array;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	array = malloc(nmemb * size);
+	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-			p[i] = 0;
 
-	return (p);
+	for (i = 0; i < (nmemb * seze); i++)
+			array[i] = 0;
+
+	return (array);
 }
