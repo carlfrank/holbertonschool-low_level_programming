374d0705fb3372f7e132e7ca633d8a51e632c039
carlfrank <carl-frank7@hotmail.com>
2023-07-14 00:09:31 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,41 +12,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	int strlen1, i, c;
+	char *ptr;
+	int i, j;
 
 	if (s1 == NULL)
-			s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-			s2 = "";
+		s2 = "";
 
-	strlen1 = (unsigned int)_strlen1(s1);
-	p = malloc ((strlen1 + n + 1) * sizeof(char));
-	if (p == NULL)
-			return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (i < strlen1)
-				p[i] = s1[i];
-		else
-				p[i] = s2[c++];
+		for (j = 0; s2[2] != '\0'; j++)
 	}
-	p[i] = '\0';
 
-	return (p);
-}
+	if (n > j)
+		n = j;
 
-/**
- * _strlen - find lenght of string
- * @s: string
- * Return: lenght of string
- */
+	ptr = malloc ((i + n) + 1);
 
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-			;
-	return (i);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		ptr[j] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
