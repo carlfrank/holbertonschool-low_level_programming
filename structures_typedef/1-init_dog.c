dcb4510142907b5fe24531eaae59d540beba5446
carlfrank <carl-frank7@hotmail.com>
2023-07-17 14:38:51 +0000

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,11 +11,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name;
-	d -> age;
-	d -> owner;
-
-	return (0);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 };
 
 #endif
