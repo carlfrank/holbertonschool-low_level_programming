a56b5c29608eea2f3016f33e2d8998343033b960
carlfrank <carl-frank7@hotmail.com>
2023-07-17 13:26:26 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog -Define a new type struct dog with the following elements:
+ * @name: char1
+ * @age: float
+ * @owner: char2
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
