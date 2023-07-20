fb9cb2ab4273f7531f6358fc138ef03a1b4d7e25
carlfrank <carl-frank7@hotmail.com>
2023-07-20 14:44:25 +0000

    3-calc.h

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -24,10 +24,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (s == ops[i].op)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 			
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
