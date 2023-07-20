439a4b064cc13dc6d77b4c8a0e78efe28c6f7fae
carlfrank <carl-frank7@hotmail.com>
2023-07-20 14:34:24 +0000

    full-3exercise

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main (int argc, char *argv[])
 		exit(99);
 	}
 
-	if (argv[2] == "/" || argv[2] == "%" && atoi(argv[3]) == 0)
+	if (atoi(argv[3]) == 0)
 	{
 		printf ("error\n");
 		exit(100);
