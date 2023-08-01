9ffd4d470bdbfbe8760a71716c15053fc8a1b9ac
carlfrank <carl-frank7@hotmail.com>
2023-08-01 16:04:21 -0400

    Add Node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,45 +1,38 @@
 #include "lists.h"
-
 /**
- * add_node_end - Write a function that adds a new node
- * @head: double pointer to head
- * @str:  needs to be duplicated
- * Return: the address of the new element, or NULL if it failed
+ * add_node_end - adds a new node at the end of a list
+ * @head: point to the beginning
+ * @str: point to the string
+ * Return: the address of the new elements, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *current_node = *head;
+	list_t *adding, *last;
 
-	char *duplicated_str = strdup(str);
+	adding = malloc(sizeof(list_t));
 
-	new_node = malloc(sizeof(list_t));
-
-	if (new_node == NULL)
+	if (adding == NULL || str == NULL)
 	{
 		return (NULL);
 	}
 
-	if (duplicated_str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	adding->str = strdup(str);
+	adding->len = strlen(str);
+	adding->next = NULL;
 
-	new_node->str = duplicated_str;
-	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new_node;
+		*head = adding;
+		return (adding);
 	}
 
-	while (current_node->next != NULL)
-	{
-		current_node->next = current_node->next;
-	}
+	last = *head;
 
-	current_node->next = new_node;
-	return (new_node);
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = adding;
+
+	return (adding);
 }
