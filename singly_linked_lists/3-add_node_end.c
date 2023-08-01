33bd407d70e55f6b653ea7919c881e89b8020dc8
carlfrank <carl-frank7@hotmail.com>
2023-08-01 10:20:21 -0400

    Add Note

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,18 +9,19 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-    list_t *current_node;
-    char *duplicated_str;
+	list_t *new_node ;
+	list_t *current_node = *head;
 
-	*new_node = malloc(sizeof(list_t));
+	char *duplicated_str = strdup(str);;
+
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
-	*duplicated_str = strdup(str);
+
 
 	if (duplicated_str == NULL)
 	{
@@ -35,15 +36,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		*head = new_node;
 	}
-	else
-	{
-		*current_node = *head;
-	}
+
 	while (current_node->next != NULL)
 
 	{
-		current_node-> = new_node;
+		current_node-> next = current_node->next;
 	}
+
+	current_node->next=new_node;
 	return new_node;
 
 }
