e355da51cf1a94595777c49a163f2ba5e60958f8
carlfrank <carl-frank7@hotmail.com>
2023-08-10 16:42:07 -0400

    hash table print

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *node;
+	char *sep;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	sep = "";
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
