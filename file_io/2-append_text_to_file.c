6299dcd3ed1636ea477fed4f02f5f01c14ed333a
carlfrank <carl-frank7@hotmail.com>
2023-08-10 09:22:13 -0400

    Append text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,3 +6,26 @@
  * Return: 1 on success and -1 on failure, if filename is null return -1
  */
 
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		write(fd, text_content, len);
+		close(fd);
+	}
+	return (1);
+}
