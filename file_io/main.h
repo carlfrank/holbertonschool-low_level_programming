85c3ebe09fd9e99511dd5fa4174e4e7ab6dcac6e
carlfrank <carl-frank7@hotmail.com>
2023-08-11 09:39:11 -0400

    3cp

--- file_io/main.h
+++ file_io/main.h
@@ -11,5 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_and_exit(const char *message, const char *filename, int exit_code);
+void argc_fail(void);
 
 #endif
